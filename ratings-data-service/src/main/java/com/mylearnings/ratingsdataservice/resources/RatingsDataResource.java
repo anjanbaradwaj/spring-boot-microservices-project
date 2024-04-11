639ea6862d225210f4f777291723f24c74bcd69e
anjanbaradwaj <anjan.baradwaj4@gmail.com>
2024-04-11 12:55:58 +0530

    Spring Boot Microservice Project
    Created 3 microservices using Spring Boot along with one more microservice which is enabled as a Eureka Server for Service Discovery
    
    The data is hard-coded, for test and practice purposes

---
+++ ratings-data-service/src/main/java/com/mylearnings/ratingsdataservice/resources/RatingsDataResource.java
@@ -0,0 +1,32 @@
+package com.mylearnings.ratingsdataservice.resources;
+
+import com.mylearnings.ratingsdataservice.models.Rating;
+import com.mylearnings.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+    @GetMapping("/{movieId}")
+    public Rating getRating(@PathVariable String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("LotR", 5),
+                new Rating("Hobbit", 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRatings(ratings);
+        return userRating;
+    }
+}
